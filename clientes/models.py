622d8e0f483d8be2ed7ce58b0a37868b603e0f25
95Caique <decaiquesouza@gmail.com>
2024-05-16 19:51:49 -0300

    adição de models e criação de clientes e carros

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=100)
+    email = models.EmailField()
+    cpf = models.CharField(max_length=11)
 
-# Create your models here.
+    def __str__(self) -> str:
+        return self.nome
+
+class Carro(models.Model):
+    carro = models.CharField(max_length=50)
+    ano = models.IntegerField()
+    placa = models.CharField(max_length=20)
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    lavagens = models.IntegerField(default=0)
+    consertos = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.nome
+
--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,51 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import re
+from datetime import datetime
+
+
+from .models import Cliente, Carro
+
+
 def clientes(request):
-    return render(request,'clientes.html')
+    if request.method == "GET":
+        return render(request, 'clientes.html', {'data': datetime(day=22, month=3, year=2015, hour=10, microsecond=2)})
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placas')
+        anos = request.POST.getlist('ano')
+
+        cliente = Cliente.objects.filter(cpf=cpf)
+
+        if cliente.exists():
+            #TODO: Adicionar mensagem
+
+            return HttpResponse('Cliente já cadastrado')
+
+        if not re.fullmatch(re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+'), email):
+            # TODO: Adicionar mensagens
+            return HttpResponse('Email inválido')
+
+    cliente = Cliente(
+            nome = nome,
+            sobrenome = sobrenome,
+            email = email,
+            cpf = cpf
+        )
+    cliente.save()
+
+    for carro, placa, ano in zip(carros, placas, anos):
+        carro = Carro(carro=carro, placa=placa, ano=ano, cliente=cliente)
+        carro.save()
+
+        #Renderizar template
+        return HttpResponse('teste')
+    for carro, placa, anos in zip(carros, placas, anos):
+        car = Carro(carro=carro, placa=placa, ano=anos, cliente=cliente)
+        car.save
+
+
